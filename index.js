9714762df406046a65f1d553fc854131cd78cb68
dien.tt <thanhdien25598@gmail.com>
2021-04-17 17:32:48 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// require: call express library from node_modules folder 
+const express = require('express') 
+const app = express()
+const port = 3000
+
+app.get('/tin-tuc', (req, res) => {
+    var a = 1;
+    var b = 2;
+
+    var c = a + b;
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
